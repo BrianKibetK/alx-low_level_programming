093699df930db7fcf12523a1d466f65fb3f78c06
BrianKigen <brianrui723@gmail.com>
2022-03-11 05:20:54 -0800

    Print with function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 
+/**
+ * main Entry point (Print using puts function)
+ *
+ * Return: Always 0 (Success), non-zero (Failed)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");	
-	getchar();
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
