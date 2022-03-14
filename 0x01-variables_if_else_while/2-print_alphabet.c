1aa8b0a63daf9ecb93522106d2596f9956f9ba5b
BrianKigen <brianrui723@gmail.com>
2022-03-14 05:10:00 -0700

    Assign number to the variable and print the last digit stored in n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints the alphabet in lower case
+ * Hint: use while loop for this
+ * Return: Always 0 (Success), non-zero (Failed)
+ */
+int main(void)
+{
+	char ch = a;
+
+	putchar(ch);
+	return (0);
+}
