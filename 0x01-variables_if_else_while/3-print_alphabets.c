c52a834237ae8361b45464c24d83c8b01ba2f9d8
BrianKigen <brianrui723@gmail.com>
2022-03-14 07:57:38 -0700

    Print in lowercase then uppercase followed by new line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,20 +3,36 @@
 /**
  * main - Prints alphabets in lower case, then uppercase, then a new line
  * only allowed to use putchar (thrice in your code)
+ * Both for Loop and while loop
  * Return: Always 0 (Success), non-zero (Failed)
- */
+ */ 
+/*For Loop*/
+
 int main(void)
 {
-	char ch;
+/*	char ch;
 	char CH;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
+ 	{
 		putchar(ch);
 	}
 	for (CH = 'A'; CH <= 'Z'; CH++)
 	{
 		putchar(CH);
+	}*/
+
+	char ch = 'a', CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
 	}
 	putchar('\n');
 	return (0);
