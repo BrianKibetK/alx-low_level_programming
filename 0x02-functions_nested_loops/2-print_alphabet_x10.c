c0d6ec22e448f4ea8805afb8d177d7f1ad03833d
Brian Kirui <brianrui723@gmail.com>
2022-03-17 20:32:54 +0300

    My commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet
+ * only use _putchar twice
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alph;
+
+	while (i < 10)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
