f463f9f7c1101492a824c59c1505b9180bbe408e
Brian Kirui <brianrui723@gmail.com>
2022-03-17 21:24:46 +0300

    My commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,14 @@
  * @c: Character to be checked
  * Return: 1 if lower or uppercase, 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+	{
+		return (1);
+	}
+	esle
+	{
+		return (0);
+	}
 }
