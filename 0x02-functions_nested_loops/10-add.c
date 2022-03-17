9938fca25d54aec5391623d0ba340478c0bba633
Brian Kirui <brianrui723@gmail.com>
2022-03-18 00:03:36 +0300

    My commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function adding two integers
+ * @n1: First integer
+ * @n2: Second integer
+ * Return: Always 0 (Success)
+ */
+int add(int n1, int n2)
+{
+	int resuelt;
+
+	result = n1 + n2;
+	return (result);
+}
+
+
