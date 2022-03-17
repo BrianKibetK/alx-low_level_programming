060b3b465d4acf66fe4d98006d9b26e073cbd2a2
Brian Kirui <brianrui723@gmail.com>
2022-03-17 23:53:39 +0300

    My commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - Print the 9 times table starting with 0
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			res = i * j;
+			if (j == 0)
+			{
+				_putchar(res + '0');
+			}
+			if (res < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(res + '0');
+			}
+			else if (res >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
