20cd9f8c0f2e338afcc52df645b93192e72adc91
Brian Kirui <brianrui723@gmail.com>
2022-03-17 21:59:17 +0300

    Absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of integer
+ *
+ * Return: Always 0 (Success)
+ */
+int abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n)
+	}
+	else
+	{
+	 return (n);
+	}
+}
