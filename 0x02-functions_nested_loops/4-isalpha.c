b6bb0076eb08722f56e069f0dfd74eaf1e94c982
Brian Kirui <brianrui723@gmail.com>
2022-03-17 21:27:32 +0300

    My commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,5 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
-	{
-		return (1);
-	}
-	esle
-	{
-		return (0);
-	}
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
