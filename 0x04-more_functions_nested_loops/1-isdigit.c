7c76009c17b890134bb7540a59182b7e251d1a2c
Brian Kirui <brianrui723@gmail.com>
2022-03-18 17:33:52 +0300

    My commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - Check for digit (0 through 9)
- * @c: character
+ * @c: test character
  * Return: 1 if digit, 0 otherwise
  */
 
@@ -10,6 +10,7 @@ int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
+	else
 	return(0);
 }
 
