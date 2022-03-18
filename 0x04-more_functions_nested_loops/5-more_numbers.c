98dabeec31369c081fdb697add8b142754a27180
Brian Kirui <brianrui723@gmail.com>
2022-03-18 20:23:28 +0300

    Print numbers ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,37 @@
 #include "main.h"
 
 /**
- * more_numbers - print numbers 0-14 ten times
+ * more_numbers - prints numbers 0-14 ten times
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 void more_numbers(void)
 {
-	int i, n;
+	int line, n, repeat, limit;
 
-	for (i = 0; i <= 9; i++)
+	n = '0';
+	repeat = 0;
+	limit = '9';
+
+	for (line = 0; line < 10; line++)
 	{
-		for (n = '0'; n < '15'; n++)
+		while (repeat < 2)
 		{
-			_putchar(n);
+			while (n <= limit)
+			{
+				if (limit == '4')
+					_putchar('1');
+				_putchar(n);
+				n++;
+			}
+			repeat++;
+			limit = '4';
+			n = '0';
 		}
 		_putchar('\n');
+		repeat = 0;
+		n = '0';
+		limit = '9';
 	}
 }
