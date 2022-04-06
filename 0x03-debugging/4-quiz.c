38db699f921d9439fc658008019086637556e4ad
Brian Kirui <brianrui723@gmail.com>
2022-04-06 23:10:09 +0300

    Static Libraries

---
+++ 0x03-debugging/4-quiz.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+/**
+ * main - output of code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+/**	int i;
+ *
+ *	for (i = 48; i < 58; i++)
+ *	{
+ *		printf("%c", i);
+ *	}
+ *	printf("\n");
+ */
+/**	int i;
+ *
+ *	i = 0;
+ *
+ *	while (i < 10)
+ *	{
+ * 		i++;
+ *		printf("%d", i/2);
+ *	}
+ */
+/**	int i;
+ *
+ *	for (i = 0; i < 10; i++)
+ *	{
+ *		printf("%d", i * 2);
+ *	}
+ */
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		i++;
+		printf("%d", i / 2);
+	}
+}
---
+++ 0x03-debugging/5-quiz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - quiz
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 9;
+	while (i--)
+	{
+		printf("%d", i);
+	}
+}
