5f16df1464d0793465151f807f6ce4c1d20df1f2
Brian Kirui <brianrui723@gmail.com>
2022-04-08 12:45:58 +0300

    Static LIbraries

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -c -Wall -Werror -Wextra *.c
+gcc -c -Wall -Werror -Wextra 0-isupper.c 0-strcat.c 1-isdigit.c 1-strncat.c 2-strlen.c 3-islower.c 3-strcmp.c 4-isalpha.c 5-strstr.c 9-strcpy.c _putchar.c
 ar -rc liball.a *.o
 ranlib liball.a
