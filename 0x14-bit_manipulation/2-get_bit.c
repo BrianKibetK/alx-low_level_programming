8667904463aeabd2c9627db66de4446f13b8de10
Brian Kirui <brianrui723@gmail.com>
2022-04-29 22:53:50 +0300

    Get value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,14 +9,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	for (index = 0; index < 33; index++)
-	{
-		if (index > 32)
+		if (index >= (sizeof(unsigned long int) * 8))
 			return (-1);
 
-		if ((n & index) == 0)
+		if ((n & (1 << index)) == 0)
 			return (0);
 
 		return (1);
-	{
 }
